a400bd9f08dd2225cb017c0d1bc2a5112839d730
Wonjune <103029974+one3147@users.noreply.github.com>
2023-07-21 15:10:43 +0900

    Update TokenLimit.sol

--- ERC20/TokenLimit.sol
+++ ERC20/TokenLimit.sol
@@ -1,1 +1,19 @@
+// SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.13;
+contract MyToken is Ownable,ERC20Capped {
+    constructor(string memory name, string memory symbol, uint256 initialSupply,uint256 _cap) ERC20(name,symbol) ERC20Capped(_cap*10**uint(decimals())){
+        _mint(msg.sender,initialSupply*10**uint(decimals()));
+    }
 
+    function AdditionallCoinSupply(uint AddTokenSupply) public onlyOwner{
+        _mint(msg.sender,AddTokenSupply*10**uint(decimals())); // decimal = 18
+    }
+
+    function ToAddCoin(address _to, uint AddToken) public onlyOwner{
+        _mint(msg.sender,AddToken*10**uint(decimals()));
+        _transfer(msg.sender,_to, AddToken);
+    }
+}
