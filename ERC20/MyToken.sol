752bcf567f060d85808b315a9e8e418b7c3c4c43
Wonjune <103029974+one3147@users.noreply.github.com>
2023-07-21 10:43:58 +0900

    Update MyToken.sol

--- ERC20/MyToken.sol
+++ ERC20/MyToken.sol
@@ -1,1 +1,8 @@
-.
+// SPDX-License-Identifier: MIT
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+pragma solidity ^0.8.13;
+contract MyToken is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name,symbol) {
+        _mint(msg.sender,initialSupply*10**uint(decimals()));
+    }
+}
